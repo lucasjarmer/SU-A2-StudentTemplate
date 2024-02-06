f5169cca9f527d36269577d9cdf1728a438bf1c3
Lucas Jarmer <dnh463@alumni.ku.dk>
2024-02-06 14:40:16 +0100

    added skeleton for tests

---
+++ DIKUArenaTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+using DIKUArena;
+namespace DIKUArenaTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        #
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
