c72682c89c3b7436e00de4d0d1eb34e5c89fb6cc
laufeykaritas <61380860+laufeykaritas@users.noreply.github.com>
2024-02-08 22:05:03 +0100

    Update UnitTest1.cs

--- DIKUArenaTests/UnitTest1.cs
+++ DIKUArenaTests/UnitTest1.cs
@@ -1,18 +1,55 @@
 using NUnit.Framework;
 using DIKUArena;
-namespace DIKUArenaTests;
 
-public class Tests
+namespace DIKUArenaTests
 {
-    [SetUp]
-    public void Setup()
-    {
-        #
-    }
+    public class UnitTest1 {
+        private Arena arena;
+        private Traex traex;
+        private Samnite samnite;
+        private Retiarii retiarii;
+        private Gladiator glad;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void Setup() {
+            arena = new Arena();
+            traex = new Traex("Trata");
+            samnite = new Samnite("Samna");
+            retiarii = new Retiarii("Retata");
+            glad = new Gladiator("glad");
+        }
+
+        [Test]
+        public void TestArenaInitialization() {
+            Assert.IsNotNull(arena);
+        }
+        
+        [Test] 
+        public void TestBattle(){
+            Gladiator res = arena.Battle(traex, samnite);
+            Assert.IsNotNull(res);
+        }
+        
+        [Test] 
+        public void onlyOneLost(){
+            arena.Battle(traex, samnite);
+            Assert.Multiple(() =>{
+                Assert.IsFalse(traex.HasLost() && samnite.HasLost());  // Both can not lose
+                Assert.IsTrue(!traex.HasLost() || !samnite.HasLost()); // One must win
+            });
+        }
+        
+        [Test]
+        public void TestRunTournament() {
+            Gladiator[] instances = {traex, samnite, retiarii, glad};
+            List<Gladiator> players = new List<Gladiator>(instances);
+
+            Gladiator res = arena.RunTournament(players);
+
+            Assert.IsNotNull(res);
+        }
+
+        // Due to non-specificality in protection level of fields/properties, there are no 
+        // Further explicit tests
     }
 }
